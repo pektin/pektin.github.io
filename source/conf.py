d157adea10b229fca7361a81352b9e9486dcfe3a
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-03-23 16:24:22 +1100

    Added inheriting of sphinx build settings from jam repository

--- source/conf.py
+++ source/conf.py
@@ -1,41 +1,9 @@
 import sys
 import os
 
-
-
-#-- Temporary until importing the other config file works
-
 # Inherit configuration from the jam docs
-#sys.path.append(os.getcwd())
-
-#from jam.conf import *
-
-extensions = [
-    'sphinx.ext.todo',
-]
-
-# The suffix of source filenames.
-source_suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
-
-# General information about the project.
-project = 'Jam'
-
-# The short X.Y version.
-version = '0.0'
-# The full version, including alpha/beta/rc tags.
-release = '0.0.1'
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
-# Keep warnings as "system message" paragraphs in the built documents.
-keep_warnings = True
-
-#--
-
+sys.path.append(os.getcwd())
+from jam.conf import *
 
 import sphinx_bootstrap_theme
 
